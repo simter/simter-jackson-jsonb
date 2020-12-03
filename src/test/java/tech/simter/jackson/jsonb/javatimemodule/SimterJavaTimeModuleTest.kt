949902b5a3eb364efe033eb44bea3f7aa8222d56
RJ <rongjihuang@gmail.com>
2020-12-03 15:40:26 +0800

    Add unit test for simter and jackson JavaTimeModule

---
+++ src/test/java/tech/simter/jackson/jsonb/javatimemodule/SimterJavaTimeModuleTest.kt
@@ -0,0 +1,74 @@
+package tech.simter.jackson.jsonb.javatimemodule
+
+import net.javacrumbs.jsonunit.assertj.JsonAssertion
+import net.javacrumbs.jsonunit.assertj.JsonAssertions.assertThatJson
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.format.annotation.DateTimeFormat
+import tech.simter.jackson.jsonb.JacksonJsonbUtils
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+import javax.json.bind.Jsonb
+import javax.json.bind.JsonbBuilder
+
+/**
+ * Test JSON-B serialize JavaTime with simter JavaTimeModule.
+ *
+ * @author RJ
+ */
+class SimterJavaTimeModuleTest @Autowired constructor() {
+  data class DateTimeExample(
+    val localDate: LocalDate,
+    @get:DateTimeFormat(pattern = "yyyy年MM月dd日") // 自定义格式，使用 JsonFormat 也可以
+    val localDate2: LocalDate,
+    val localDate3: LocalDate?, // 允许空值
+    val localTime: LocalTime,
+    val localDateTime: LocalDateTime
+  )
+
+  private val now = OffsetDateTime.now().truncatedTo(ChronoUnit.SECONDS)!!
+  private val now2Day = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd"))!!
+  private val now2Minute = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm"))!!
+  private val now2Second = now.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))!!
+  private val now2Cn = now.format(DateTimeFormatter.ofPattern("yyyy年MM月dd日"))!!
+  private lateinit var jsonb: Jsonb
+
+  @Suppress("NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
+  @BeforeEach
+  fun setUp() {
+    JacksonJsonbUtils.setObjectMapper(null) // remove ObjectMapper instance cache
+    jsonb = JsonbBuilder.create()!!
+  }
+
+  @Test
+  fun `single value`() {
+    val localDateTime = LocalDateTime.of(2010, 1, 2, 13, 20, 30)
+    assertEquals("\"2010-01-02 13:20\"", JsonbBuilder.create().toJson(localDateTime))
+  }
+
+  @Test
+  fun `bean value`() {
+    // init data
+    val example = DateTimeExample(
+      localDate = now.toLocalDate(),
+      localDate2 = now.toLocalDate(),
+      localDate3 = null,
+      localTime = now.toLocalTime(),
+      localDateTime = now.toLocalDateTime()
+    )
+    val json = jsonb.toJson(example)
+    assertThatJson(json).and(
+      JsonAssertion { it.node("localDate").isEqualTo(now2Day) }, // yyyy-MM-dd
+      JsonAssertion { it.node("localDate2").isEqualTo(now2Cn) }, // yyyy年MM月dd日
+      JsonAssertion { it.node("localDate3").isAbsent() }, // 空值默认不输出
+      JsonAssertion { it.node("localTime").isEqualTo(now2Second.substring(11, 16)) }, // HH:mm
+      JsonAssertion { it.node("localDateTime").isEqualTo(now2Minute) } // yyyy-MM-dd HH:mm
+    )
+  }
+}
